22cadccf3b0ff9d17803d5601b276e7bc117c26e
robin <makenenjoy@gmail.com>
2023-11-26 00:17:32 +0100

    reduced long line

--- tic-solved/src/main.rs
+++ tic-solved/src/main.rs
@@ -4,11 +4,11 @@ mod minmax;
 mod game_terminal_interface;
 
 use tic::*;
-use minmax::*;
+use minmax::MinimaxPlayer1Strategy as P1;
 use game_terminal_interface::*;
 
 fn main() {
     let board = Board::new();
-    let strategy: MinimaxPlayer1Strategy<Board, AlmostWinHeuristic> = MinimaxPlayer1Strategy::new(5);
+    let strategy: P1<Board, AlmostWinHeuristic> = P1::new(5);
     run_bot_player1_vs_human_player2(board, strategy);
 }
